750a4f3c3f2e7a3643b6b6ced1e118c7094429aa
SchroedingersCat <alex.egger96@gmail.com>
2013-01-15 16:32:45 +0100

    Full commit. Includes libraries, test classes and test resources.

---
+++ src/com/test/state/TestState.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.test.state;
+
+import com.lambda.GameState;
+
+/**
+ * @author alex
+ *
+ */
+public class TestState extends GameState {
+
+	/* (non-Javadoc)
+	 * @see com.engine.GameState#init()
+	 */
+	@Override
+	public void init() {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.engine.GameState#render()
+	 */
+	@Override
+	public void render() {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.engine.GameState#update(int)
+	 */
+	@Override
+	public void update(int delta) {
+	}
+
+}
